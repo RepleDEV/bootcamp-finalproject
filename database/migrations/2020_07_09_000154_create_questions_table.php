74649f1eea7b8c493fb8842539ce2faa818e754e
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-07-09 07:43:20 +0700

    Initial commit

---
+++ database/migrations/2020_07_09_000154_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('content', 255);
+            $table->string('tags', 255);
+            $table->date('created_at', 255);
+            $table->date('updated_at', 255);
+            $table->bigInteger('points');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
